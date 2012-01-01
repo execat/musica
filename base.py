2f784d96ffc2f9bf0dbe23b2d80b5c255f6bdae7
Anuj More <anujmorex@gmail.com>
2012-01-01 23:28:07 +0530

    Final commit for the day. Shorter, more efficient code

--- base.py
+++ base.py
@@ -1,27 +1,31 @@
-#!/usr/bin/python2
+#!/usr/bin/env python
 
 notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
+#Index to note
+def i_to_n(index):
+	return notes[index%12]
+
+#Note to index
+def n_to_i(note):
+	return notes.index(note)
+
+#Major, minor, and other scales.
 def major(root):
-	start = notes.index(root)
-	return [notes[i] for i in ((0 + start)%12, (2 + start)%12, (4 +   \
-		start)%12, (5 + start)%12, (7 + start)%12, (9 + start)%12,\
-		(11 + start)%12, (12 + start)%12)]
+	return [i_to_n(i + n_to_i(root)) for i in (0, 2, 4, 5, 7, 9, 11)]
 
 def minor(root):
-	start = notes.index(root)
-	return [notes[i] for i in ((0 + start)%12, (2 + start)%12, (3 +   \
-		start)%12, (5 + start)%12, (7 + start)%12, (8 + start)%12,\
-		(10 + start)%12, (12 + start)%12)]
+	return [i_to_n(i + n_to_i(root)) for i in (0, 2, 3, 5, 7, 8, 10)]
 
 def maj_penta(root):
-	return [major(root)[i] for i in (0, 1, 2, 4, 5, 7)]
+	return [major(root)[i] for i in (0, 1, 2, 4, 5)]
 
 def min_penta(root):
-	return [minor(root)[i] for i in (0, 2, 3, 5, 6, 7)]
+	return [minor(root)[i] for i in (0, 2, 3, 5, 6)]
 
 def blues(root):
 	ret = min_penta(root)
 	ret.insert(3, notes[(notes.index(root) + 6) % 12])
 	return ret
 
+print (blues('A'))
