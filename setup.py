c7c89d0ad5ff119966de094e7ef76aba132a2349
Anuj More <anujmorex@gmail.com>
2013-07-04 22:42:13 +0530

    Maybe unpolished version of setup.py from http://guide.python-distribute.org/quickstart.html

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='musica',
+    version='0.0.1',
+    packages=['musica',],
+    license='GNU General Public License version 3',
+    long_description=open('README.txt').read(),
+)
