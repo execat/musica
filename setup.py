59e491555cd55c3e302861aa9cd2d5df87459932
Anuj More <anujmorex@gmail.com>
2013-07-07 09:07:09 +0530

    Changes to setup.py from Abjad

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from distutils.core import setup
 setup(
     name='musica',
     version='0.0.1',
+    description = 'musica is a Python tool for working with musical notations and scores.',
+    author = 'Anuj More',
+    author_email = 'anujmorex@gmail.com',
+    url = 'https://github.com/execat/musica',
+    keywords = 'music composition',
     packages=['musica',],
-    license='GNU General Public License version 3',
+    license='GPL',
     long_description=open('README.txt').read(),
 )
